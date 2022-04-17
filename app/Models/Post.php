b61de5db6d17ff4b9a219f6d8da29e2e726ba824
Feruzkarimov <feruzbekkarimov02@gmail.com>
2022-04-17 14:43:53 +0500

    added relations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable =['id', 'name','user_id'];
+
+    public function user () {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $fillable = ['id', 'name'];
+
+    public function post () {
+        return $this->hasMany(Post::class);
+    }
 }
